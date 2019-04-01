e96ad36cc8f7c5462ad6668be09f0b30ff37e3c2
manadmishra <manadmishra98@gmail.com>
2019-04-02 01:49:00 +0530

    CSS modifications 2

--- profile.php
+++ profile.php
@@ -22,5 +22,51 @@ $query = "SELECT * FROM APPLICANT_ WHERE email_id='$email'";
 $result = mysqli_query($con, $query);
 $row = mysqli_fetch_assoc($result);
 // echo $row['date_of_birth'];
-echo "<html><body><br><br><center><h3>Edit Profile</h3><form action = \"profile-backend.php\" method = \"POST\">Name: <input name = \"name\" value = \"{$row['username']}\"><br><br>Date Of Birth: <input name = \"date_of_birth\" value = \"{$row['date_of_birth']}\" type = \"date\"><br><br>Current Role: <input name = \"curr_role\" value = \"{$row['role']}\"><br><br><button type = \"Update\">Submit</button></form></center></body></html>";
+echo 
+"
+<html>
+<head>
+	<meta charset=\"UTF-8\">
+	<title>Login Page</title>
+	
+	
+	
+		<link rel=\"stylesheet\" href=\"css/style.css\">
+
+	
+</head>
+
+
+<body>
+<br><br>
+<center><h3>Edit Profile</h3>
+<div class=\"wrapper\">
+		<div class=\"container\">
+		<h1>Edit Profile</h1>
+		<form class = \"form\" action = \"profile-backend.php\" method = \"POST\">
+		Name: <input name = \"name\" value = \"{$row['username']}\"><br><br>
+		Date Of Birth: <input name = \"date_of_birth\" value = \"{$row['date_of_birth']}\" type = \"date\"><br><br>
+		Current Role: <input name = \"curr_role\" value = \"{$row['role']}\"><br><br>
+		<button type = \"Update\">Submit</button>
+		</form>
+		</div>
+		
+		<ul class=\"bg-bubbles\">
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+		</ul>
+	</div>
+</center>
+</body>
+</html>
+"
+;
 ?>
