8cc2f842abd9abeaf3c797625e1cf4a28e8f5529
Eeshan Gupta <eeshan9815@gmail.com>
2019-04-02 03:41:22 +0530

    org home and add listing

---
+++ org_new_listing.php
@@ -0,0 +1,54 @@
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>New Listing</title>
+	
+	
+	
+		<link rel="stylesheet" href="css/style.css">
+
+	
+</head>
+<body>
+	<div class="wrapper">
+		<div class="container">
+			<h1>Login</h1>
+			<form class="form" action = "org_new_listing_backend.php" method = "POST">
+            Organization: <select name="org_reg">
+			<?php 
+			require_once 'dbconnect.php';
+			$sql = mysqli_query($con, "SELECT org_reg FROM ORGANIZATION_") or die(mysql_error());
+			while ($row = $sql->fetch_assoc()){
+			echo $row['org_reg'];
+			echo "<option value=\"{$row['org_reg']}\">" . $row['org_reg'] . "</option>";
+			// \"{$row['mobile_no']}\"
+			}
+			?>
+            </select><br>
+            Role: <input name = "role" placeholder = "role"><br><br>
+			Tag: <input name = "tag" placeholder = "tag"><br><br>
+            Location: <input name = "location" placeholder = "location"><br><br>          
+            Experience Required: <input name = "expreq" placeholder = "number of years"><br><br>
+            Open Date: <input name = "start" type = "date"><br><br>			
+			Close Date: <input name = "end" type = "date"><br><br>			
+
+            <!-- <input name = "email" placeholder = "Email/Registration ID"><br><br> -->
+			<button type = "submit">Submit</button>
+			</form>
+		</div>
+		
+		<ul class="bg-bubbles">
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+		</ul>
+	</div>
+</body>
+</html>
---
+++ org_new_listing_backend.php
@@ -0,0 +1,29 @@
+<?php
+/*
+$host = "localhost";
+$user = "USER_NAME";
+$dbpass = "PASSWORD";
+$dbname = "DB_NAME";
+$con = mysqli_connect($host,$user,$dbpass,$dbname);
+*/
+require_once 'dbconnect.php';
+session_start();
+$org = $_POST["org_reg"];
+// echo $org;
+$start = date('Y-m-d', strtotime($_POST["start"]));
+$end = date('Y-m-d', strtotime($_POST["end"]));
+$role = $_POST["role"];
+$tag = $_POST["tag"];
+$email = $_SESSION['email_id'];
+$location = $_POST["location"];
+$exp_req = $_POST["expreq"];
+// $query = "SELECT * FROM APPLICANT_ WHERE email_id='$email'";
+$query = "INSERT INTO JOB_LISTING_ (org_reg, role, tag, location, exp_req, open_date, close_date) VALUES('$org', '$role', '$tag', '$location', '$exp_req', '$start', '$end')";
+
+// echo $years;
+$result = mysqli_query($con, $query);
+
+echo "<br><br><br><center><h1>Listing Added</h1></center>";
+echo "<br><center><a href = \"org_home.php\">Go back</a></center>";
+
+?>
