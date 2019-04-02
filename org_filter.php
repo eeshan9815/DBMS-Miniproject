2e8f988beea152dddc23d493f0117d854d002b3e
Eeshan Gupta <eeshan9815@gmail.com>
2019-04-02 07:11:10 +0530

    minor changes

--- org_filter.php
+++ org_filter.php
@@ -14,17 +14,26 @@
 	<center>
 		<div class="wrapper">
 		<div class="container">
-		<h3>Search Candidates</h3>
+		<h1>Search Candidates</h1>
 		<form action = "org_filter_backend.php" method = "POST">
 			Enter the following details (Leave empty in case of no preference)<br><br>
        		Candidate Name: <input name = "name" placeholder = "Enter Name"><br><br>
    			Role: <input name = "role" placeholder = "Enter Role"><br><br>
 			Tag: <input name = "tag" placeholder = "Enter Tag of Candidates"><br><br>
-			Qualification: <input name = "qual" placeholder = "Enter Qualification of Candidates"><br><br>
+			
 			Skill: <input name = "skill" placeholder = "Enter Skill of Candidates"><br><br>
 			Location: <input name = "location" placeholder = "Enter Location of Candidates"><br><br>
-			Experience (Years): <input name = "exp_years" placeholder = "Enter Minimum Experience of Candidate"><br><br>
-			<button type = "submit">Submit</button>
+			Experience (Years): <input name = "exp_years" placeholder = "Enter Minimum Experience of Candidate"><br>
+            Qualification:<br> <select id="degree" name="degree">                      
+			  <option value="None">--Select Degree--</option>
+			  <option value="M.Tech">M.Tech</option>
+			  <option value="B.Tech">B.Tech</option>
+			  <option value="PHD">PHD</option>
+			  <option value="M.Sc">M.Sc</option>
+			  <option value="B.Tech+M.Tech(Dual)">B.Tech+M.Tech(Dual)</option>
+			</select><br><br>
+			
+            <button type = "submit">Submit</button>
 		</form>
 		<form action="org_home.php">
 			<button type="submit">Go Back</button>
